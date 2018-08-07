496c078c70b5b917225ac28d52ebcba40c4b540e
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-07 22:17:33 +0530

    Update MMBankController.java

--- MMBankController.java
+++ MMBankController.java
@@ -5,12 +5,15 @@ public class MMBankController extends BankController {
 	private BankCollection bc;
 	private BankFactory bf;
 	
+	//Parameterized Constructor
 	public MMBankController(BankFactory bf) {
 		super(bf);
 		this.bf=bf;
 		bc= new BankCollection();
 	}
+	
 
+	//function to create new savings account
 	@Override
 	public void createNewSavingAccount(int accNo, String accNm, float accBal, boolean isSalary) {
 		MMBankFactory bankFactory= new MMBankFactory();
@@ -19,6 +22,7 @@ public class MMBankController extends BankController {
 		bc.addNewAccount(ba);
 	}
 
+	//function to create new current account
 	@Override
 	public void createNewCurrentAccount(int accNo, String accNm, float accBal, float creditLimit) {
 		MMBankFactory bankFactory= new MMBankFactory();
@@ -26,6 +30,7 @@ public class MMBankController extends BankController {
 		bc.addNewAccount(ba);
 	}
 
+	//function to find account details based on account number
 	@Override
 	public void presentAccountToView(int accNo) {
 		for(BankAcc ba:bc.al)
