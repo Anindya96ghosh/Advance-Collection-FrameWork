7179a2977e6ef564e2421d4d419a4f134323a35e
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-07 22:26:34 +0530

    Update SavingAcc.java

--- SavingAcc.java
+++ SavingAcc.java
@@ -5,13 +5,18 @@ public abstract class SavingAcc extends BankAcc{
 	private boolean isSalary;
 	private static final float MINBAL=1000;
 	
+	
+	//Parameterized Constructor
 	public SavingAcc(int accNo, String accNm, float accBal, boolean isSalary) {
 		super(accNo, accNm, accBal);
 		this.isSalary = isSalary;
 	}
+	//Overridden withdraw method
 	@Override
 	abstract public void withdraw(float money);
 
+	
+	//Overridden toString method
 	@Override
 	public String toString() {
 		return "SavingAcc [isSalary=" + isSalary + "]";
